a6ceb20918caf41066979eedb28412e2908d06d2
TALHAA23 <talhasifat23@gmail.com>
2025-05-06 12:55:32 +0500

    cleanup

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,6 @@
-import {
-  AIButtonProps,
-  AIFormProps,
-  AIInputProps,
-} from "intelligent-react-components";
-
 export const packageRegistriesOptions = ["npm", "pnpm", "yarn"];
 export const aiComponentNames = ["AIButton", "AIInput", "AIFrom"];
 export const keywords = {
   pkgname: "Intelligent React Components",
   pkgnameAbbr: "IRC",
 };
-export type UnionType = AIInputProps | AIFormProps | AIButtonProps | null;
