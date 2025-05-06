3764408278eed3b225c4f4ec0387c61a6ff528b1
TALHAA23 <talhasifat23@gmail.com>
2025-05-06 12:45:41 +0500

    removed showcase

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,6 +3,7 @@ import { Image } from "nextra/components";
 import { useTheme } from "nextra-theme-docs";
 export default function Logo() {
   const theme = useTheme().resolvedTheme;
+  console.log(theme);
   return (
     <Image
       src={theme === "light" ? "/logo-light-mode.svg" : "/logo-dark-mode.svg"} // Corrected the path for dark mode
